2dded5969b85df97267f763166a66186328d1b2c
Renan-Silva235 <renan.rubio94@gmail.com>
2026-07-15 22:36:16 -0300

    refatoração

--- src/main/java/com/flashcards/api/services/EmailService.java
+++ src/main/java/com/flashcards/api/services/EmailService.java
@@ -1,7 +1,7 @@
 package com.flashcards.api.services;
 
-import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
@@ -11,30 +11,21 @@ public class EmailService {
 
     @Autowired
     private JavaMailSender mailSender;
-    @Autowired
-    private org.springframework.core.env.Environment environment;
-    private final Dotenv dotenv = Dotenv.load();
 
+    @Value("${SMTP_MAIL_EMAIL_CORPORATION}")
+    private String smtpMailCorporation;
 
     public void sendVerificationCode(String to, String code) {
-        String smtpMailCorporation = dotenv.get("SMTP_MAIL_EMAIL_CORPORATION");
-
         SimpleMailMessage message = new SimpleMailMessage();
-
         message.setFrom(smtpMailCorporation);
         message.setTo(to);
         message.setSubject("Código de verificação - Flash Cards");
-
         message.setText("""
             Seu código de verificação é:
-
             %s
-
             Este código expira em 7 minutos.
-
             Se você não solicitou esta operação, ignore este e-mail.
             """.formatted(code));
-
         mailSender.send(message);
     }
 }
